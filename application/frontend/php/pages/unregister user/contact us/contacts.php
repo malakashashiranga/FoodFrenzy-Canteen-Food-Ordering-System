6bd27444d68472dea32cf338e5cf78cad77fde41
malakashashiranga <malakashashiranga115@gmail.com>
2024-07-27 23:52:25 +0530

    set page common files contacts

--- application/frontend/php/pages/unregister user/contact us/contacts.php
+++ application/frontend/php/pages/unregister user/contact us/contacts.php
@@ -6,23 +6,13 @@
 <body>
 	<div class="full_screen">
 		<?php 
-			include '/xampp/htdocs/FoodFrenzy-Canteen-Food-Ordering-System/application/frontend/html/same/unregister user/navigation/navigation_bar.html';
+			include '/xampp/htdocs/FoodFrenzy-Canteen-Food-Ordering-System/application/frontend/html/common/unregister user/navigation/navigation_bar.html';
 		?>
 		
 		<?php 
-			include '/xampp/htdocs/FoodFrenzy-Canteen-Food-Ordering-System/application/frontend/html/same/register and unregister user/contact us/contact.html';
+			include '/xampp/htdocs/FoodFrenzy-Canteen-Food-Ordering-System/application/frontend/html/common/register and unregister user/contact us/contact.html';
 		?>
 	</div>
-	
-	<?php
-		include '/xampp/htdocs/FoodFrenzy-Canteen-Food-Ordering-System/application/frontend/html/same/register and unregister user/alert bar/alert.html';
-	?>
-	
-	<?php
-		include '/xampp/htdocs/FoodFrenzy-Canteen-Food-Ordering-System/application/frontend/html/same/admin, register and unregister user/loading spinner/loading_spinner.html';
-	?>
-	
-	<script src="/FoodFrenzy-Canteen-Food-Ordering-System/application/frontend/javascript/ajax/unregister user/contact us/form_submit_ajax.js"></script>
 	
 </body>
 </html>

91d5ae4309f2cbecfa18cf47f39508f1407d998a
Diluk-priyanathh <dilukpriyanath26@gmail.com>
2024-12-12 11:40:18 +0530

    added wallet history

---
+++ application/AJAX/register user/wallet/wallet history/load_wallet_history_ajax.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+    function getWalletHistoryDetails() {
+        $.ajax({
+            url: '/FoodFrenzy/application/backend/php/register user/wallet details/show wallet history/load_wallet_history.php',
+            type: "POST",
+            dataType: "json",
+            data: {},
+            success: function (data) {
+                if (data.success) {
+                    if (data.noWalletHistory) {
+						$('#walletHistoryBody').empty();
+						$(".userDetailsTable").hide();
+						$("#noItemAlert").show();
+						$("#noItemAlert").text(data.alert);
+                    } else {
+						$("#noItemAlert").hide();
+                        const pastWalletDetails = data.pastWalletDetails;
+						$('#walletHistoryBody').empty();
+                        pastWalletDetails.forEach(function(walletItem) {
+                            const row = `<tr>
+											<td style="padding: 20px 70px;">${walletItem.date}</td>
+                                            <td style="padding: 20px 70px;">${walletItem.pastBalance}</td>
+                                            <td style="padding: 20px 70px;">${walletItem.transactionType}</td>
+                                            <td style="padding: 20px 70px;">${walletItem.transactionBalance}</td>
+                                            <td style="padding: 20px 70px;">${walletItem.newBalance}</td>
+                                        </tr>`;
+                            $('#walletHistoryBody').append(row);
+                        });
+                    }
+                } else {
+                    if (data.error_page) {
+                        const encodedAlert = encodeURIComponent(data.alert);
+                        const goBackURL = "/FoodFrenzy/application/frontend/php/pages/register user/wallet history/wallet_history.php";
+                        const errorPageURL = `/FoodFrenzy/application/frontend/html/same/admin, register and unregister user/error page/error.html?alert=${encodedAlert}&goBackURL=${encodeURIComponent(goBackURL)}`;
+                        window.location.href = errorPageURL;
+                    }
+                }
+            },
+            error: function (xhr, status, error) {
+                if (data.error_page) {
+                    const encodedAlert = encodeURIComponent(data.alert);
+                    const goBackURL = "/FoodFrenzy/application/frontend/php/pages/register user/wallet history/wallet_history.php"; 
+                    const errorPageURL = `/FoodFrenzy/application/frontend/html/same/admin, register and unregister user/error page/error.html?alert=${encodedAlert}&goBackURL=${encodeURIComponent(goBackURL)}`;
+                    window.location.href = errorPageURL;
+                }
+            }
+        });
+    }
+    getWalletHistoryDetails();
+});
